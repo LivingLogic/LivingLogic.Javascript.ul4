72f19f410f4c75aaa2c1e7cce1e409f8ac94cfea
Walter Doerwald <walter@livinglogic.de>
2016-03-18 13:59:23 +0100

    Bump copyright year.

--- ul4.js
+++ ul4.js
@@ -3,8 +3,8 @@
  * http://www.livinglogic.de/Python/ul4c/
  * http://www.livinglogic.de/Python/ul4on/
  *
- * Copyright 2011-2015 by LivingLogic AG, Bayreuth/Germany
- * Copyright 2011-2015 by Walter Dörwald
+ * Copyright 2011-2016 by LivingLogic AG, Bayreuth/Germany
+ * Copyright 2011-2016 by Walter Dörwald
  *
  * All Rights Reserved
  *
